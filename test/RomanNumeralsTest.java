f68a87c8c9342900fd617844b7da7e4c090ed62e
NickZhang <looloopig@gmail.com>
2013-08-19 22:02:48 +0530

    Shoulde return IV given 4

--- test/RomanNumeralsTest.java
+++ test/RomanNumeralsTest.java
@@ -1,33 +1,38 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class RomanNumeralsTest {
+
+    private RomanNumerals romanNumerals;
+
+    @Before
+    public void setUp() throws Exception {
+        romanNumerals = new RomanNumerals();
+    }
+
     @Test
     public void shouldReturnIGiven1() {
-        RomanNumerals romanNumerals= new RomanNumerals();
         String roman = romanNumerals.roman(1);
         assertThat(roman, is("I"));
     }
 
     @Test
     public void shouldReturnIIGiven2() {
-        RomanNumerals romanNumerals= new RomanNumerals();
         String roman = romanNumerals.roman(2);
         assertThat(roman, is("II"));
     }
 
     @Test
     public void shouldReturnIIIGiven3() {
-        RomanNumerals romanNumerals= new RomanNumerals();
         String roman = romanNumerals.roman(3);
         assertThat(roman, is("III"));
     }
 
     @Test
     public void shouldReturnIVGiven4() {
-        RomanNumerals romanNumerals= new RomanNumerals();
         String roman = romanNumerals.roman(4);
         assertThat(roman, is("IV"));
     }
